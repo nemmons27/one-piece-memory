26092953d769588a3bc42625a418828067a527fe
Nicholas Emmons <emmonsnicholas7@gmail.com>
2023-05-05 11:47:55 -0400

    Memory Cards

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 //Welcome to My Code for Project 1. 
 //This code is going to generate a Memory game for anime fans, One Piece to be specific.
 
-//Constants//
-// First we're going to have to declare the cards, the game-timer, and the moves counter as our constants
+//Display message "Choose Difficulty", Display 2 buttons ["Captain", "Admiral"]
+    //If user presses "Captain", Displays 4x4 board of cards
+    //else, displays 6x6 board of cards
 
-
-
-//Variables//
-
-
-//Event Listeners//
-
-
-//Function//
-
+//Game-Timer starts on first card selection, counting down from 1:30
+//If the second card chosen doesn't match the first card, 
+    //Both cards return face down and a 5 second reduction
+    //else, both cards stay face up
+//Game-timer stops when last pair is made,
+    //if the timer runs out, Display message "Seems like you got lost"
+    //else, displays message "You might be ready to find the One Piece!"
+//Diplay time remaining
+//Display moves used
+//Display play again button

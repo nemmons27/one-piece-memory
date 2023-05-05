cb53f3552907252d9e7e897a7ed9447110b71c4c
Nicholas Emmons <emmonsnicholas7@gmail.com>
2023-05-05 11:00:45 -0400

    Memory Cards

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-//Welcome to My COde
-
+//Welcome to My Code for Project 1. 
+//This code is going to generate a Memory game for anime fans, One Piece to be specific.
 
 //Constants//
+// First we're going to have to declare the cards, the game-timer, and the moves counter as our constants
+
 
 
 //Variables//
